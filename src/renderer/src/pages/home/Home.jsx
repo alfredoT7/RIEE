62a752035e466fe8d91b870079e4d14126b69c5c
alfredoT7 <123456alfredotorri@gmail.com>
2024-10-11 23:04:38 -0400

    pacientes lista re recientes en el home terminado

--- src/renderer/src/pages/home/Home.jsx
+++ src/renderer/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import TopInfoHome from '../../components/topInfoHome/TopInfoHome'
-
+import CardPaciente from '../../components/cardPaciente/CardPaciente'
 const Home = () => {
   return (
     <section className="section-main-container">
@@ -9,12 +9,28 @@ const Home = () => {
         <TopInfoHome />
         <TopInfoHome />
       </div>
+      <h3>Novedades</h3>
       <div className="parent">
         <div className="div1"></div>
         <div className="div2"></div>
         <div className="div3"></div>
         <div className="div4"></div>
       </div>
+      <h3 className="recent-title">Pacientes Recientes</h3>
+        <div className="head-paciente-title">
+          <div className="ci"><p>CI</p></div>
+          <div className="name"><p>Nombre</p></div>
+          <div className="treatment"><p>Tratamiento</p></div>
+          <div className="date"><p>Fecha</p></div>
+        </div>
+
+        <div className="card-paciente-container">
+          <CardPaciente ci="12345600" nombre="Juan Perez" tratamiento="Ortodoncia" fecha="12/12/2021" />
+          <CardPaciente ci="1231" nombre="Juan Perez" tratamiento="Ortodoncia" fecha="12/12/2021" />
+          <CardPaciente ci="12345600" nombre="Juan Perez" tratamiento="Ortodoncia" fecha="12/12/2021" />
+          <CardPaciente ci="12345600" nombre="Juan Perez" tratamiento="Ortodoncia" fecha="12/12/2021" />
+          <CardPaciente ci="12345600" nombre="Juan Perez" tratamiento="Ortodoncia" fecha="12/12/2021" />
+        </div>  
     </section>
   );
 };
