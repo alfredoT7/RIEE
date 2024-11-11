c33b08e2d0b248615d8d670ff4d642ba9e2204fa
alfredoT7 <123456alfredotorri@gmail.com>
2024-11-11 11:50:41 -0400

    Feat:new patient form done

--- src/renderer/src/components/searchBar/SearchBar.jsx
+++ src/renderer/src/components/searchBar/SearchBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './SearchBar.css'
 import { FaSearch, FaPlus } from 'react-icons/fa'
 
 const SearchBar = () => {
+  const navigate = useNavigate()
+  const click = () => {
+    navigate('/new-patient')
+  }
+
   return (
     <div className='sb-main-cont'>
         <input type='text' placeholder='Busca un paciente por nombre o CI'/>
         <FaSearch className='icon-search'/>
-        <button>
+        <button onClick={click}>
           <p>Agrega nuevo paciente</p>
           <FaPlus className='icon-plus'/>
         </button>
